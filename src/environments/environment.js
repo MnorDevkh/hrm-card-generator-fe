0d4d1ffc65beff345fff1737c426fd833f2ef0bd
2005 <96278125+MnorDevkh@users.noreply.github.com>
2026-03-20 09:58:07 +0700

    Update environment.js

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // apiBaseUrl: 'http://localhost:8000/',
-  apiBaseUrl: 'http://localhost:8000/',
-  url: 'http://localhost:8000//'
+  apiBaseUrl: 'https://cardsystemapi.aga-institute.edu.kh/',
+  url: 'https://cardsystemapi.aga-institute.edu.kh/'
 };
